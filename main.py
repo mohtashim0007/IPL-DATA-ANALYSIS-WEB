f811c9f9bd4d8f4fb6f5331d26e25aa343869767
Mohd Mohtashim <mohtashim0007@gmail.com>
2022-07-24 15:41:32 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+fig_colors = ['Set1', 'Set2', 'Set3', 'tab10' ,'deep', 'hls', 'husl', 'rocket_r', 'YlOrBr', 'Spectral']
+
+df_matches = pd.read_csv('/content/drive/My Drive/DataSet/IPL Matches 2008-2020.csv')
+
+
+
+
+import streamlit as st
+
+with st.sidebar:
+  radio = st.radio('Main Menu : ', ('Match Stats', 'Player stats', 'Dream 11'))
+  if radio == 'Match Stats':
+    st.header('IPL Team Wise Data' )
+
+
+  elif radio == 'Player stats':
+    st.header('IPL Players Data' ) 
+
+
+  elif radio == 'Dream 11':
+    st.header('AI Based Recommended Dream11 Players' )
+
