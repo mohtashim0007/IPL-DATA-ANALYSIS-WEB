3ae678cfb0b579b1aef79989c117c4b0a2acaa7b
Mohd Mohtashim <mohtashim0007@gmail.com>
2022-07-25 08:30:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def matches_won_by_team1_team2(team1, team2):
   plt.ylabel('Matches Won')
   plt.savefig('fig1.jpg')
   img = cv2.imread('fig1.jpg')
-  img = cv2.resize(img, (380,324))
+  img = cv2.resize(img, (480,424))
   _,col,_ = st.columns([1,2,1])
   with col:
     st.image(img)
